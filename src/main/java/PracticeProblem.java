5c224b5b5a514c4ff0a57d1ff44d3ef6f29e7b4e
Nithin Alagar <343954178@tdsb.ca>
2026-04-28 11:28:03 -0400

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,15 @@
+/**
+
+        * File: Lesson 5.1 - arrays
+
+        * Author: nitin.a
+
+        * Date Created: April 28, 2026
+
+        * Date Last Modified: april 28, 2026
+
+        */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -23,5 +35,27 @@ public class PracticeProblem {
 	public static void q5() {
 		//Write question 5 code here
 	}
+
+    public static int[] createIntArray(){
+        int[] arr = new int[5];
+		for (int i = 0; i < arr.length; i++){
+		    arr[i] = i + 1;
+		}
+		return arr;
+		
+	}
+
+	public static String[] createArray(String var1, String var2, String var3 , String var4){
+	    return new String[] {var1, var2, var3, var4};
+	}
+
+	public static String getElement(int idx, String[] arr3){
+		return arr3[idx];
+	}
+
+	public static void replaceElement(int idx, double num, ndouble[] arr2){
+		arr2[idx] = num;
+	}
+		
 
 }
